d950adbaa114fd6f6b1c1962a5be04f61c693e89
Ruan Bekker <ruan.ru.bekker@gmail.com>
2023-04-19 22:39:43 +0200

    Update bitcoin-seconds-out-of-sync.sh

--- bitcoin-seconds-out-of-sync.sh
+++ bitcoin-seconds-out-of-sync.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-verification=$(curl -s -u "${rpcuser}:${rpcpass}" -d '{"jsonrpc": "1.0", "id": "curl", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://127.0.0.1:18332/ | jq -r '.result.verificationprogress')
-blockcount=$(curl -s -u "${rpcuser}:${rpcpass}" -d '{"jsonrpc": "1.0", "id": "curl", "method": "getblockcount", "params": []}' -H 'content-type: text/plain;' http://127.0.0.1:18332/ | jq -r '.result')
-blockhash=$(curl -s -u "${rpcuser}:${rpcpass}" -d "{\"jsonrpc\": \"1.0\", \"id\": \"curl\", \"method\": \"getblockhash\", \"params\": [$blockcount]}" -H 'content-type: text/plain;' http://127.0.0.1:18332/ | jq -r '.result')
-time=$(curl -s -u "${rpcuser}:${rpcpass}" -d '{"jsonrpc": "1.0", "id": "curl", "method": "getblock", "params": ["'"$blockhash"'"]}' -H 'content-type: text/plain;' http://127.0.0.1:18332/ | jq -r '.result.time')
-echo $(((`date +%s`-$time)/60))
+#verification=$(curl -s -u "${rpcuser}:${rpcpass}" -d '{"jsonrpc": "1.0", "id": "curl", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://127.0.0.1:18332/ | jq -r '.result.verificationprogress')
+#blockcount=$(curl -s -u "${rpcuser}:${rpcpass}" -d '{"jsonrpc": "1.0", "id": "curl", "method": "getblockcount", "params": []}' -H 'content-type: text/plain;' http://127.0.0.1:18332/ | jq -r '.result')
+#blockhash=$(curl -s -u "${rpcuser}:${rpcpass}" -d "{\"jsonrpc\": \"1.0\", \"id\": \"curl\", \"method\": \"getblockhash\", \"params\": [$blockcount]}" -H 'content-type: text/plain;' http://127.0.0.1:18332/ | jq -r '.result')
+#time=$(curl -s -u "${rpcuser}:${rpcpass}" -d '{"jsonrpc": "1.0", "id": "curl", "method": "getblock", "params": ["'"$blockhash"'"]}' -H 'content-type: text/plain;' http://127.0.0.1:18332/ | jq -r '.result.time')
+#echo $(((`date +%s`-$time)/60))
+
+median=$(curl -s -u "bitcoinrpc:bitcoinpass" -d '{"jsonrpc": "1.0", "id": "bitcoind", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://127.0.0.1:18332/ | jq -r '.result.mediantime')
+seconds=$(echo $(date +%s) - $median | bc )
+echo $((seconds/86400))" days "$(date -d "1970-01-01 + $seconds seconds" "+%H hours %M minutes %S seconds")
